92e04c8ab20e249f21dd55b5361289a8b0e16136
Akash <akashbytes@gmail.com>
2024-03-15 13:26:57 +0530

    new question added ....

--- index.js
+++ index.js
@@ -50,3 +50,84 @@ Ques3d.innerText = Dates.toLocaleString()
 // ------------------------------------------------------------------------
 
 // Q --- 4 ----------------------------------------------------------------
+
+let wellCome = document.getElementById("Q4")
+let textNode = wellCome.childNodes[0]
+let text = textNode.data
+// console.log(textNode)
+
+
+setInterval(()=>{
+    text=text[text.length-1]+ text.substring(0, text.length - 1);
+    textNode.data = text;
+},200)
+
+// ------------------------------------------------------------------------
+
+// Q --- 4 ----------------------------------------------------------------
+
+function LeapYear(year){
+    if(year/100 !== 0  && year/4){
+        alert("yes"+year+" it's a leap year !")
+    }else{
+        alert("No"+year+" it's not  a leap year")
+    }
+}
+
+// LeapYear(2016)
+// ------------------------------------------------------------------------
+
+// Q --- 5 ----------------------------------------------------------------
+
+
+// let months = ["January"," February", "March", "April", "May", "June", "July", "August"," September", "October", "November","December"]
+
+for(let year = 2014 ; year<= 2025 ; year ++){
+ let date = new Date(year,0,1)
+ if(date.getDate()===0){
+    console.log("1st January is being a Sunday  " + year)
+ }
+}
+
+/// leet code q---1 
+let ShowHW = document.getElementById("ShowHW")
+console.log(ShowHW)
+function helloWorld (){
+    console.log("Hello World ...")
+  ShowHW.innerText="Hello World"
+}
+
+function ShowHelloWorld(){
+    helloWorld ()
+}
+
+
+// Q -- 2Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+let num = 0 
+
+for( num ; num <= 10 ; num++){
+    console.log(num)
+}
+
+// Q -- 3 Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+// -- toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+// -- notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+
+function expacted(val){
+    if (val === 5){
+        let obj = {
+            val:"true"
+        }
+        console.log( obj)
+    }else if (val !== 5){
+        let objj = {
+            val:"Not Equal"
+        }
+        console.log( objj)
+    }
+
+}
+
+expacted(7)
+expacted(5)

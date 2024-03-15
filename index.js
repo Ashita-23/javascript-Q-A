daf58966c2432e1fe584da64a2ae06ae30a5ada5
Akash <akashbytes@gmail.com>
2024-03-15 15:47:07 +0530

    two more ques...

--- index.js
+++ index.js
@@ -131,3 +131,94 @@ function expacted(val){
 
 expacted(7)
 expacted(5)
+
+//Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+
+//The three functions are:
+//-- increment() increases the current value by 1 and then returns it.
+//-- decrement() reduces the current value by 1 and then returns it.
+//-- reset() sets the current value to init and then returns it.
+
+
+let counter = 0 
+let textCount = document.getElementById("counterText")
+textCount.innerText=counter
+
+function incrementCounter(){
+counter++
+textCount.innerText=counter
+}
+function decrementCounter(){
+    if(counter >0){
+        counter--
+        textCount.innerText=counter
+    }
+}
+
+function reset(){
+    counter = 0
+    textCount.innerText=counter
+
+}
+
+// Q -- 4Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+//The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+//Please solve it without the built-in Array.map method.
+
+let arrayOne = [1,2,3,4,5,6,]
+
+
+function setmap(arr,val){
+    let newArray = []
+        for(let x of arr ){
+            newAs= x*val
+            console.log(newAs)
+            newArray.push(newAs)
+      }
+      console.log(newArray,"..")
+}
+
+setmap(arrayOne,2)
+
+
+//Q --  Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
+//The fn function takes one or two arguments:
+//arr[i] - number from the arr
+//i - index of arr[i]
+//filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value. A truthy value is a value where Boolean(value) returns true.
+//Please solve it without the built-in Array.filter method.
+
+let arrayForFilter = [10,3,5,6,32,25,65,45,23,12,100,56,89,90]
+
+function filterFun(arr,con){
+    let newArr = []
+    for(let x of arr){
+   if(x>con){
+//    console.log(x,"x")
+   newArr.push(x)
+   }
+    }
+    console.log(newArr,"NA")
+}
+filterFun(arrayForFilter,23)
+//Q --6 Given an integer array nums, a reducer function fn, and an initial value init, return the final result obtained by executing the fn function on each element of the array, sequentially, passing in the return value from the calculation on the preceding element.
+//This result is achieved through the following operations: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The ultimate value of val is then returned.
+//If the length of the array is 0, the function should return init.
+//Please solve it without using the built-in Array.reduce method.
+
+let  arrayForReducer = [1,2,3,4,5,]
+
+function arrayReducerFn(array){
+let sum = 1
+
+for (let i = 1; i <array.length;i++){
+
+    sum = sum*array[i]
+    
+}
+console.log(sum)
+
+}
+arrayReducerFn(arrayForReducer)
